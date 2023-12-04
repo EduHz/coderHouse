47cbf320e62b30b06199c2bddcbe8d2e971b5ad2
Edu <eduhzprogramador@gmail.com>
2023-12-04 16:36:46 -0300

    update check

--- App.js
+++ App.js
@@ -1,53 +1,52 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
-const App = () => {
-  const [textItem, setTextItem] = useState("");
-  const [itemList, setItemList] = useState([]);
+export default function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  console.log(list.map((x) => x));
+
+  const handleSubmit = () => {
+    setList([...list, { text: item, checked: false }]);
+    setItem("");
+  };
+
+  const handleDelete = (index) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
+  const handleCheck = (index) => {
+    const newList = [...list];
+    newList[index].checked = !newList[index].checked;
+    setList(newList);
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputAdd}>
-        <TextInput style={styles.input} placeholder="Ingrese Item" />
-        <Button title="Add" onPress={() => setTextItem(textItem)} />
+    <View>
+      <Text>Primera Lista Sin Ayudas</Text>
+      <View>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          placeholder="Add item..."
+          value={item}
+          onChangeText={(text) => setItem(text)}
+        />
+        <Button title="Add" onPress={handleSubmit} />
       </View>
-      <View style={styles.inputList}>
-        <Text>texto 1</Text>
-        <Button title="Delete" onPress={() => console.log("Botón Delete")} />
-      </View>
+      {list.map((item, index) => (
+        <View key={index}>
+          <Text
+            style={item.checked ? { textDecorationLine: "line-through" } : {}}
+          >
+            {item.text}
+          </Text>
+          <Button title="Delete" onPress={() => handleDelete(index)} />
+          <Button title="Check" onPress={() => handleCheck(index)} />
+        </View>
+      ))}
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    justifyContent: "start-end",
-    alignItems: "center",
-  },
-  inputAdd: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    width: "100%",
-  },
-  inputList: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    width: "100%",
-  },
-  input: {
-    flex: 1,
-    marginRight: 10,
-    height: 40,
-    borderWidth: 1,
-    paddingHorizontal: 10,
-  },
-});
-
-export default App;
+}
